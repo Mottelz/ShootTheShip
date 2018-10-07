5a1205e6d0bf1347eea3de4f9d8689190cd7a8f3
Mottel Zirkind <shachna@gmail.com>
2018-10-07 00:44:32 -0400

    Fixed the movement for enemy2.

--- Assets/Scripts/MoveByRB2D.cs
+++ Assets/Scripts/MoveByRB2D.cs
@@ -9,13 +9,15 @@ namespace Mottel {
     public class MoveByRB2D : MonoBehaviour {
 		public float speed;
 		public MovementType movement;
-		private Rigidbody2D rb;
 		public Boundry bounds;
+        private Rigidbody2D rb;
+		private float startTime;
         
 		/// <summary>
         /// Sets the simple velocity, based on the selected MovementType or calls ComplexMovement().
         /// </summary>
         private void Start() {
+            startTime = Time.fixedTime;
 			rb = GetComponent<Rigidbody2D>();
 			if(movement == MovementType.Down) {
 				rb.velocity = Vector2.down * speed;
@@ -25,45 +27,35 @@ namespace Mottel {
 				rb.velocity = new Vector2(0.13f, 1.0f) * speed;
 			} else if(movement == MovementType.RightBullet) {
 				rb.velocity = new Vector2(-0.13f, 1.0f) * speed;
-			} else {
-				StartCoroutine(ComplexMovement());
+			} else if (movement == MovementType.Boss) {
+                StartCoroutine(ComplexMovement());
+			} else if (movement == MovementType.Centipede) { 
+				rb.velocity = Vector2.down;
 			}
+		}
+
+		private void FixedUpdate() {
+			float eslapsed = Time.fixedTime - startTime;
+            if (movement == MovementType.Centipede) {
+				rb.AddForce(Vector2.left * Mathf.Cos(eslapsed/0.85f) * speed, ForceMode2D.Force);
+            }
 		}
 
         /// <summary>
         /// Complex movement that requires pauses over time. Decided based on some basic math.
         /// </summary>
         IEnumerator ComplexMovement() {
-			if(movement == MovementType.Centipede) {
-				while(true) {
-					//move left
-					rb.velocity = Vector2.left * speed;
-					float time = Mathf.Abs(transform.position.x - bounds.xMin) / speed;
-					yield return new WaitForSeconds(time);
-					//move down
-					rb.velocity = Vector2.down * speed;
-					yield return new WaitForSeconds(0.3f);
-					//move right
-					rb.velocity = Vector2.right * speed;
-					time = Mathf.Abs(transform.position.x - bounds.xMax) / speed;
-					yield return new WaitForSeconds(time);
-					//move down
-					rb.velocity = Vector2.down * speed;
-					yield return new WaitForSeconds(0.3f);
-				}
-			} else if (movement == MovementType.Boss) {
-				rb.velocity = Vector2.down * speed;
-				yield return new WaitForSeconds(1.5f);
-				while(true) {
-					//move left
-					rb.velocity = Vector2.left * speed;
-					float time = Mathf.Abs(transform.position.x - bounds.xMin) / speed;
-					yield return new WaitForSeconds(time);
-					//move right
-					rb.velocity = Vector2.right * speed;
-					time = Mathf.Abs(transform.position.x - bounds.xMax) / speed;
-					yield return new WaitForSeconds(time);
-				}
+			rb.velocity = Vector2.down * speed;
+			yield return new WaitForSeconds(1.5f);
+			while(true) {
+				//move left
+				rb.velocity = Vector2.left * speed;
+				float time = Mathf.Abs(transform.position.x - bounds.xMin) / speed;
+				yield return new WaitForSeconds(time);
+				//move right
+				rb.velocity = Vector2.right * speed;
+				time = Mathf.Abs(transform.position.x - bounds.xMax) / speed;
+				yield return new WaitForSeconds(time);
 			}
 		}
 	}
